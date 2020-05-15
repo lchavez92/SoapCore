ba1b659559d9510faed669fd68066ab6a17af54c
Leonel Chavez <lchavez@qconsultores.pe>
2020-05-14 20:22:07 -0500

    SkipImportNamespaces option added for SAP SOAP Consumer compatibility

--- src/SoapCore/SoapOptions.cs
+++ src/SoapCore/SoapOptions.cs
@@ -5,57 +5,59 @@ using SoapCore.Extensibility;
 
 namespace SoapCore
 {
-	public class SoapOptions
-	{
-		public Type ServiceType { get; set; }
-		public string Path { get; set; }
-		public SoapEncoderOptions[] EncoderOptions { get; set; }
-		public SoapSerializer SoapSerializer { get; set; }
-		public bool CaseInsensitivePath { get; set; }
-		public ISoapModelBounder SoapModelBounder { get; set; }
-		public Binding Binding { get; set; }
-		public int BufferThreshold { get; set; } = 1024 * 30;
-		public long BufferLimit { get; set; }
+    public class SoapOptions
+    {
+        public Type ServiceType { get; set; }
+        public string Path { get; set; }
+        public SoapEncoderOptions[] EncoderOptions { get; set; }
+        public SoapSerializer SoapSerializer { get; set; }
+        public bool CaseInsensitivePath { get; set; }
+        public ISoapModelBounder SoapModelBounder { get; set; }
+        public Binding Binding { get; set; }
+        public int BufferThreshold { get; set; } = 1024 * 30;
+        public long BufferLimit { get; set; }
 
-		/// <summary>
-		/// Gets or sets a value indicating whether publication of service metadata on HTTP GET request is activated
-		/// <para>Defaults to true</para>
-		/// </summary>
-		public bool HttpGetEnabled { get; set; } = true;
+        public bool SkipImportNamespaces { get; set; }
 
-		/// <summary>
-		/// Gets or sets a value indicating whether publication of service metadata on HTTPS GET request is activated
-		/// <para>Defaults to true</para>
-		/// </summary>
-		public bool HttpsGetEnabled { get; set; } = true;
+        /// <summary>
+        /// Gets or sets a value indicating whether publication of service metadata on HTTP GET request is activated
+        /// <para>Defaults to true</para>
+        /// </summary>
+        public bool HttpGetEnabled { get; set; } = true;
 
-		public bool OmitXmlDeclaration { get; set; } = true;
+        /// <summary>
+        /// Gets or sets a value indicating whether publication of service metadata on HTTPS GET request is activated
+        /// <para>Defaults to true</para>
+        /// </summary>
+        public bool HttpsGetEnabled { get; set; } = true;
 
-		public bool IndentXml { get; set; } = true;
+        public bool OmitXmlDeclaration { get; set; } = true;
 
-		public XmlNamespaceManager XmlNamespacePrefixOverrides { get; set; }
+        public bool IndentXml { get; set; } = true;
 
-		public static SoapOptions FromSoapCoreOptions<T>(SoapCoreOptions opt)
-		{
-			var soapOptions = new SoapOptions
-			{
-				ServiceType = typeof(T),
-				Path = opt.Path,
-				EncoderOptions = opt.EncoderOptions,
-				SoapSerializer = opt.SoapSerializer,
-				CaseInsensitivePath = opt.CaseInsensitivePath,
-				SoapModelBounder = opt.SoapModelBounder,
-				Binding = opt.Binding,
-				BufferThreshold = opt.BufferThreshold,
-				BufferLimit = opt.BufferLimit,
-				HttpsGetEnabled = opt.HttpsGetEnabled,
-				HttpGetEnabled = opt.HttpGetEnabled,
-				OmitXmlDeclaration = opt.OmitXmlDeclaration,
-				IndentXml = opt.IndentXml,
-				XmlNamespacePrefixOverrides = opt.XmlNamespacePrefixOverrides
-			};
+        public XmlNamespaceManager XmlNamespacePrefixOverrides { get; set; }
 
-			return soapOptions;
-		}
-	}
+        public static SoapOptions FromSoapCoreOptions<T>(SoapCoreOptions opt)
+        {
+            var soapOptions = new SoapOptions
+            {
+                ServiceType = typeof(T),
+                Path = opt.Path,
+                EncoderOptions = opt.EncoderOptions,
+                SoapSerializer = opt.SoapSerializer,
+                CaseInsensitivePath = opt.CaseInsensitivePath,
+                SoapModelBounder = opt.SoapModelBounder,
+                Binding = opt.Binding,
+                BufferThreshold = opt.BufferThreshold,
+                BufferLimit = opt.BufferLimit,
+                HttpsGetEnabled = opt.HttpsGetEnabled,
+                HttpGetEnabled = opt.HttpGetEnabled,
+                OmitXmlDeclaration = opt.OmitXmlDeclaration,
+                IndentXml = opt.IndentXml,
+                XmlNamespacePrefixOverrides = opt.XmlNamespacePrefixOverrides
+            };
+
+            return soapOptions;
+        }
+    }
 }
